906404ec14ec7d26f7a71e21dab188288b57d337
grftwhre <44138690+grftwhre@users.noreply.github.com>
2018-12-26 18:59:16 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+using namespace std;
+struct point
+{
+ int x;
+ int y;
+};
+int range(point a,point b)
+{
+    int r=((a.x-b.x)*(a.x-b.x))+((a.y-b.y)*(a.y-b.y));
+    int res=sqrt(r);
+    return res;
+}
+int main()
+{
+    int n;
+    int m;
+    printf("enter number of points\n");
+    scanf("%d",&n);
+    point arr[n] ={};
+     for (int k=0;k<n;k++)
+       {
+        scanf("%d",&arr[k].x);
+        scanf("%d",&arr[k].y);
+       }
+       m=range(arr[1],arr[2]);
+      for (int i=0;i<n;i++)
+       {
+        printf("%d;%d\n",arr[i].x,arr[i].y);
+        for (int l=0;l<n;l++)
+        {
+            if ((range(arr[i],arr[l])<m)&&((arr[i].x!=arr[l].x)))
+            {
+             if (arr[l].y!=arr[i].y)
+             m=range(arr[i],arr[l]);
+            }
+        }
+       }
+   point q;
+   point r;
+   q.x=1;
+   q.y=4;
+   r.x=5;
+   r.y=3;
+   printf("%d",range(q,r));
+    printf("min range = %d",m);
+    return 0;
+}
